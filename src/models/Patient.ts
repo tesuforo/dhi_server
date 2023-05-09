8a78cf12affea159dbf89f8dc7ffc93a5a96de55
Jeisson Vargas <jeisson@mentu.co>
2023-05-09 18:52:15 -0500

    se agrega interface mongo para paciente

--- src/models/Patient.ts
+++ src/models/Patient.ts
@@ -6,24 +6,39 @@ import { AuthInstance as mongoose } from "..";
 import { Document } from "mongoose";
 
 export interface IPaciente extends Document {
+  tipoDocumento: "CC" | "CE";
+  documento: string;
   nombre: string;
   apellido: string;
+  fechaNacimiento: Date;
   edad: number;
   genero: "Masculino" | "Femenino" | "Otro";
   email: string;
   telefono: string;
   ciudad: string;
+  distrito: string;
+  pais: string;
+  zonaResidencial: string;
   eps: string;
+  estadoCivil: string;
+  ocupacion?: string;
+  acudiente?: string;
   fechaRegistro?: Date;
 }
 
 const pacienteSchema = new mongoose.Schema({
+  tipoDocumento: { type: String, enum: ["CC", "CE"], required: true },
+  documento: { type: String, required: true, unique: true },
   nombre: {
     type: String,
     required: true,
   },
   apellido: {
     type: String,
+    required: true,
+  },
+  fechaNacimiento: {
+    type: Date,
     required: true,
   },
   edad: {
@@ -48,9 +63,33 @@ const pacienteSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  distrito: {
+    type: String,
+    required: true,
+  },
+  pais: {
+    type: String,
+    required: true,
+  },
+  zonaResidencial: {
+    type: String,
+    required: true,
+  },
   eps: {
     type: String,
     required: true,
+  },
+  estadoCivil: {
+    type: String,
+    required: true,
+  },
+  ocupacion: {
+    type: String,
+    required: false,
+  },
+  acudiente: {
+    type: String,
+    required: false,
   },
   fechaRegistro: {
     type: Date,
