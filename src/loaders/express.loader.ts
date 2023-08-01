bd65c9892993adef0d7a5c4b47db80aec293eb67
Jeisson Vargas <javargasm91@gmail.com>
2023-07-31 19:18:20 -0500

    config start project

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -22,7 +22,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 export default (app: Application): void => {
-    app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT ?? 3000);
     app.use(cookieParser(), express.json(), express.text(), upload.single('file'));
 
     const swaggerSpec = swaggerJSDoc(options);
@@ -30,7 +30,7 @@ export default (app: Application): void => {
 
     // CORS
     app.use((req: Request, res: Response, next: NextFunction) => {
-        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+        res.header('Access-Control-Allow-Origin', req.headers.origin ?? '*');
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,9 @@
 import { Application } from 'express';
 import express from './express.loader';
-import directus from './directus.loader';
+//import directus from './directus.loader';
 import routes from './routes.loader';
 
 export default (app: Application): void => {
-    directus();
     express(app);
     routes(app);
 };
