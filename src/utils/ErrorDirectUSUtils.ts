ea6a79be7dbb7535213cf71ff96470cc9ee02105
Jeisson Vargas <javargasm91@gmail.com>
2023-08-25 19:24:45 -0500

    Validate Error DirectUS

--- src/utils/ErrorDirectUSUtils.ts
+++ src/utils/ErrorDirectUSUtils.ts
@@ -1,44 +1,9 @@
+import { isDirectusError as isDirectusErrorR } from 'utils';
+
 export interface DirectusError<Extensions = void> extends Error {
     extensions: Extensions;
-    code: string;
-    status: number;
+    message: string;
 }
-
-export interface DirectusErrorConstructor<Extensions = void> {
-    new (
-        extensions: Extensions,
-        options?: ErrorOptions,
-    ): DirectusError<Extensions>;
-    readonly prototype: DirectusError<Extensions>;
-}
-
-export const createError = <Extensions = void>(
-    code: string,
-    message: string | ((extensions: Extensions) => string),
-    status = 500,
-): DirectusErrorConstructor<Extensions> => {
-    return class extends Error implements DirectusError<Extensions> {
-        override name = 'DirectusError';
-        extensions: Extensions;
-        code = code.toUpperCase();
-        status = status;
-
-        constructor(extensions: Extensions, options?: ErrorOptions) {
-            const msg =
-                typeof message === 'string'
-                    ? message
-                    : message(extensions as Extensions);
-
-            super(msg, options);
-
-            this.extensions = extensions;
-        }
-
-        override toString() {
-            return `${this.name} [${this.code}]: ${this.message}`;
-        }
-    };
-};
 
 /**
  * Check whether or not a passed thing is a valid Directus error
@@ -46,15 +11,20 @@ export const createError = <Extensions = void>(
  * @param err - Any input
  */
 export const isDirectusError = <T = unknown>(
-    err: unknown,
+    err: any,
     code?: string,
 ): err is DirectusError<T> => {
+    if (Array.isArray(err?.errors)) {
+        for (const error of err?.errors) {
+            return isDirectusErrorR(error);
+        }
+    }
     const isDirectusError =
         typeof err === 'object' &&
         err !== null &&
         Array.isArray(err) === false &&
-        'name' in err &&
-        err.name === 'DirectusError';
+        'extensions' in err &&
+        err.extensions;
 
     if (code) {
         return (
